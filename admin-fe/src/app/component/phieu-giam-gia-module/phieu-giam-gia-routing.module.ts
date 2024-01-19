31e28cb7fd7d7d8af096756d7b9404b5d3d9dc08
binhphamdz <2020phambinh@gmail.com>
2024-01-19 09:29:46 +0700

    binh commnit routing phieu giam gia

--- admin-fe/src/app/component/phieu-giam-gia-module/phieu-giam-gia-routing.module.ts
+++ admin-fe/src/app/component/phieu-giam-gia-module/phieu-giam-gia-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DanhSachPhieuComponent} from "./danh-sach-phieu/danh-sach-phieu.component";
 
-const phieuGiamGiaRoutes: Routes = [];
+const phieuGiamGiaRoutes: Routes = [
+  {path:"phieu-giam-gia/ds-phieu-giam-gia",
+    component:DanhSachPhieuComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(phieuGiamGiaRoutes)],

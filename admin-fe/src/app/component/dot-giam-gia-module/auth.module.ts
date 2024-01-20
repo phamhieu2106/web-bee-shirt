32bd7d5512108f21f9bdbfe394130b53a0037af2
phamvanhieu2106 <phamvanhieu2106@gmail.com>
2024-01-20 10:53:32 +0700

    Add dot giam gia component

--- admin-fe/src/app/component/dot-giam-gia-module/auth.module.ts
+++ admin-fe/src/app/component/dot-giam-gia-module/auth.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 
 import { DotGiamGiaRoutingModule } from './dot-giam-gia-routing.module';
+import { DotGiamGiaComponent } from './dot-giam-gia/dot-giam-gia.component';
+import { FilterComponent } from './filter/filter.component';
+import { TableComponent } from './table/table.component';
+import { FormAddComponent } from './form-add/form-add.component';
+import { FormUpdateComponent } from './form-update/form-update.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DotGiamGiaComponent,
+    FilterComponent,
+    TableComponent,
+    FormAddComponent,
+    FormUpdateComponent
+  ],
   imports: [DotGiamGiaRoutingModule],
 })
 export class DotGiamGiaModule {}
---
+++ admin-fe/src/app/component/dot-giam-gia-module/form-update/form-update.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FormUpdateComponent } from './form-update.component';
+
+describe('FormUpdateComponent', () => {
+  let component: FormUpdateComponent;
+  let fixture: ComponentFixture<FormUpdateComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [FormUpdateComponent]
+    });
+    fixture = TestBed.createComponent(FormUpdateComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

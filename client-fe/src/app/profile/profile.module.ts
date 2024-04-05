cd7dc2ca5cad1a856a721d67a02bd5cbb15bd681
VuNguyenHaJava2102 <vunguyenhariven@gmail.com>
2024-04-05 20:30:02 +0700

    update client

---
+++ client-fe/src/app/profile/profile.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import { ProfileRoutingModule } from "./profile-routing.module";
+import { MyAccountComponent } from "./my-account/my-account.component";
+import { MyAddressesComponent } from "./my-addresses/my-addresses.component";
+import { ProfileSidebarComponent } from "./profile-sidebar/profile-sidebar.component";
+import { MyOrdersComponent } from "./my-orders/my-orders.component";
+import { OrderTrackingComponent } from "./order-tracking/order-tracking.component";
+import { MyDiscountsComponent } from "./my-discounts/my-discounts.component";
+import { PasswordComponent } from "./password/password.component";
+
+@NgModule({
+  declarations: [
+    MyAccountComponent,
+    MyAddressesComponent,
+    ProfileSidebarComponent,
+    MyOrdersComponent,
+    OrderTrackingComponent,
+    MyDiscountsComponent,
+    PasswordComponent,
+  ],
+  imports: [CommonModule, ProfileRoutingModule],
+})
+export class ProfileModule {}

76f3bd10d312c70dfbc34f2bd4f8ad0a852090d3
binhphamdz <2020phambinh@gmail.com>
2024-04-06 13:38:00 +0700

    bình commnit giao diện profile

--- client-fe/src/app/component/tai-khoan/khach-hang/khach-hang.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "app-khach-hang",
-  templateUrl: "./khach-hang.component.html",
-  styleUrls: ["./khach-hang.component.css"],
-})
-export class KhachHangComponent {}
--- client-fe/src/app/profile/my-orders/my-orders.component.ts
+++ client-fe/src/app/profile/my-orders/my-orders.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-my-orders',
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./my-orders.component.css']
 })
 export class MyOrdersComponent {
+  constructor() { }
+  ngOnInit(): void {
+    // Hiển thị trang London mặc định khi trang được tải
+    this.openCity(null, 'tatca');
+  }
+  public activeCity: string = 'tatca';
 
+  public openCity(event: MouseEvent, cityName: string): void {
+    // Ngăn chặn sự kiện mặc định khi nhấp vào nút (nếu được gọi từ sự kiện click)
+    if (event) {
+      event.preventDefault();
+    }
+
+    // Ẩn tất cả các tab content
+    const tabContents = document.getElementsByClassName('tabcontent');
+    for (let i = 0; i < tabContents.length; i++) {
+      const tabContent = tabContents[i] as HTMLElement;
+      tabContent.style.display = 'none';
+    }
+
+    // Xóa lớp active khỏi tất cả các tablinks
+    const tabLinks = document.getElementsByClassName('tablinks');
+    for (let i = 0; i < tabLinks.length; i++) {
+      const tabLink = tabLinks[i] as HTMLElement;
+      tabLink.classList.remove('active');
+    }
+
+    // Hiển thị tab content tương ứng với cityName
+    const cityElement = document.getElementById(cityName);
+    if (cityElement) {
+      cityElement.style.display = 'block';
+    }
+
+    // Thêm lớp active cho tablink tương ứng
+    const activeTabLink = document.querySelector(`.tablinks[onclick="openCity(event, '${cityName}')"]`);
+    if (activeTabLink) {
+      activeTabLink.classList.add('active');
+    }
+  }
 }
